dff5aefac4696c4afe564b5479bd7b352326e0e1
kysch13 <124356972+kysch13@users.noreply.github.com>
2024-08-12 21:46:27 -0400

    Create object for calculator - WiP

--- calculator.js
+++ calculator.js
@@ -1,10 +1,162 @@
 const calc = document.querySelector('.calculator');
-const display = document.querySelector('#display');
+const display = document.querySelector('#display-text');
 const keyPad = calc.querySelectorAll('.keypad > div');
+
+const calculator = {
+    numA: false,
+    numB: false,
+    operator: false,
+    result: false,
+    pi: false,
+    decimal: false,
+    currentNum: 'numA',
+    displayText: [],
+    updateScreen() {
+        display.textContent = this.displayText.join('');
+    },
+    clear() {
+        this.numA = false;
+        this.numB = false;
+        this.operator = false;
+        this.result = false;
+        this.pi = false;
+        this.decimal = false;
+        this.displayText = [];
+        display.textContent = '0';
+        this.currentNum = 'numA';
+    },
+    inputNum(val) {
+        if (this.displayText.length < 10 && this.pi === false) {
+            this.displayText.push(val);
+            this.updateScreen();
+        } else if (this.pi === true) {
+            this.displayText = [val];
+            this.updateScreen;
+        }
+        calculator[calculator.currentNum] = Number(this.displayText.join(''));
+    },
+    inputPi() {
+        calculator.pi = true;
+        this.displayText = ['3.14159'];
+        this.updateScreen();
+        calculator[calculator.currentNum] = 3.14159;
+    },
+    inputDecimal() {
+        if (calculator.decimal === false) {
+            this.inputNum('.');
+            calculator.decimal = true;
+        }
+    },
+    inputOperator(op) {
+        this.operator = op;
+        this.swapCurrentNum();
+        this.pi = false;
+        this.displayText = [];
+    },
+    erase() {
+        if (this.pi === false) {
+            if (this.displayText[this.displayText.length-1] === '.') {
+                this.decimal = false;   
+            }
+            this.displayText.pop();
+            if (this.displayText.length < 1) {
+                this.displayText.push('0');
+            }
+            this.updateScreen();
+            calculator[calculator.currentNum] = Number(this.displayText.join(''));
+        }
+        
+    },
+    swapCurrentNum() {
+        if (this.currentNum === 'numA') {
+            this.currentNum = 'numB';
+        } else {
+            this.currentNum = 'numA';
+        }
+        this.decimal = false;
+    },
+    calculate(a, o, b) {
+        if (typeof a === 'number' && o && typeof b === 'number') {
+            switch(o) {
+                case ('+'):
+                    this.result = a + b;
+                    break;
+                case ('-'):
+                    this.result = a - b;
+                    break;
+                case ('*'):
+                    this.result = a * b;
+                    break;
+                case ('/'): 
+                    if (b === 0) {
+                        this.result = 'Error!';
+                    } else {
+                        this.result = a / b;
+                    }
+                    break;
+            }
+            this.displayText = [];
+            this.displayText.push(this.result);
+            this.updateScreen();
+            this.numA = Number(this.result);
+            this.numB = Number(this.result);
+            this.currentNum = 'numA';
+            this.result = false;
+            this.displayText = [];
+        }
+
+
+    }
+}
+
+keyPad.forEach(key => {
+    key.addEventListener('click', (e) => {
+        clickedNode = e.currentTarget;
+        keyVal = clickedNode.dataset.keyVal;
+        keyType = clickedNode.dataset.keyType;
+        if (keyType === 'num') {
+            calculator.inputNum(keyVal);
+        } else if (keyVal === 'backspace') {
+            calculator.erase();
+        } else if (keyVal === 'add') {
+            calculator.inputOperator('+');
+        } else if (keyVal === 'subtract') {
+            calculator.inputOperator('-');
+        } else if (keyVal === 'multiply') {
+            calculator.inputOperator('*');
+        } else if (keyVal === 'divide') {
+            calculator.inputOperator('/');
+        } else if (keyVal === 'equals') {
+            calculator.calculate(calculator.numA, calculator.operator, calculator.numB);
+        } else if (keyVal === 'pi') {
+            calculator.inputPi();
+        } else if (keyVal === 'decimal') {
+            calculator.inputDecimal();
+        }
+
+
+        // Button animations
+        if (keyVal === 'C') {
+            clickedNode.classList.add('keyPressActiveAC');
+            clickedNode.onanimationend = () => {clickedNode.classList.remove('keyPressActiveAC')}
+            calculator.clear();
+        } else {
+            clickedNode.classList.add('keyPressActive');
+            clickedNode.onanimationend = () => {clickedNode.classList.remove('keyPressActive')}
+        }
+        
+    });
+})
+
 window.addEventListener('keydown', (e) => {
     keySelection(e, 'keydown');    
 })
 
+function keySelection(e, eventType) {
+    
+}
+
+/*
 keyPad.forEach(key => {
     key.addEventListener('click', (e) => {
         keySelection(e.currentTarget.id, 'click');
@@ -31,18 +183,22 @@ function keySelection(e, eventType) {
         case ('a'):
             keyID = '#key-A';
             keyActiveClassName = 'keyPressActiveAC';
+            calculator.clear();
             break;
         case ('A'):
             keyID = '#key-A';
             keyActiveClassName = 'keyPressActiveAC';
+            calculator.clear();
             break;
         case ('c'):
             keyID = '#key-C';
-            keyActiveClassName = 'keyPressActiveAC';            
+            keyActiveClassName = 'keyPressActiveAC';
+            calculator.clear();            
             break;
         case ('C'):
             keyID = '#key-C';
             keyActiveClassName = 'keyPressActiveAC';
+            calculator.clear();
             break;
         case ('x'):
             keyID = '#key-multiply';
@@ -70,17 +226,26 @@ function keySelection(e, eventType) {
             break;
         default:
             keyID = `#key-${keyChoice}`;
+            calculator.inputNum(keyChoice);
             break;
 
     }
     keyPressed = document.querySelector(keyID);
     keyPressActiveStd(keyPressed, keyActiveClassName);
-    console.log(keyID);
+    
 }
 
 function keyPressActiveStd(keyPressed, activeClassName) {
+    let keyVal;
     if (keyPressed) {
         keyPressed.classList.add(activeClassName);
         keyPressed.onanimationend = () => {keyPressed.classList.remove(activeClassName)}
     }
+    if (keyPressed.dataset.keyVal) {
+        keyVal = keyPressed.dataset.keyVal;
+
+    }
 }
+*/
+
+
